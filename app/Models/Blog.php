e04e542007ce9f3de62111adbab9b4bdba164e3f
anandaayur <as.anandaayu@gmail.com>
2024-11-22 20:05:21 +0700

    update

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
+    //use HasFactory;
     protected $fillable = [
         'judul',
         'isi',
-        'pembuat'
+        'pembuat',
+        'foto'
     ];
 }
